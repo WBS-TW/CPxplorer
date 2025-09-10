7fa6d7ce90c95c9d3400a76ae30bd767bee6bcbb
WBS-TW <37210875+WBS-TW@users.noreply.github.com>
2025-09-10 13:03:20 +0200

    added files again

---
+++ R/CPquant_plots.R
@@ -0,0 +1,322 @@
+
+#############################################################################
+plot_skyline_output <- function(Skyline_output){
+
+    Skyline_output |>
+        dplyr::filter(Isotope_Label_Type == "Quan") |>
+        dplyr::mutate(OrderedMolecule = factor(Molecule, levels = unique(Molecule[order(C_number, Cl_number)]))) |>  # Create a composite ordering factor
+        plotly::plot_ly(
+            x = ~ OrderedMolecule,
+            y = ~ Area,
+            color = ~ Sample_Type,
+            type = "box",
+            text = ~paste(
+                "Homologue: ", PCA,
+                "<br>Sample: ", Replicate_Name,
+                "<br>Area:", round(Area, 2)
+            ),
+            hoverinfo = "text"
+        ) |>
+        plotly::layout(xaxis = list(title = 'Molecule'),
+               yaxis = list(title = 'Area'))
+}
+
+#############################################################################
+
+plot_calibration_curves <- function(CPs_standards, quantUnit) {
+    # Unnest the data first
+    CPs_standards_unnested <- CPs_standards |>
+        dplyr::filter(RF > 0) |>
+        tidyr::unnest(data) |>
+        dplyr::mutate(Molecule = factor(Molecule,
+                                        levels = unique(Molecule[order(C_number, Cl_number)])))
+
+    # Get unique Quantification Groups
+    groups <- unique(CPs_standards_unnested$Quantification_Group)
+
+    # Create a list to store individual plots
+    plot_list <- list()
+
+    # Create individual plots for each group
+    for(i in seq_along(groups)) {
+        group_data <- CPs_standards_unnested |>
+            dplyr::filter(Quantification_Group == groups[i])
+
+        plot_list[[i]] <- plotly::plot_ly() |>
+            plotly::add_trace(
+                data = group_data,
+                x = ~Analyte_Concentration,
+                y = ~Area,
+                color = ~PCA,
+                type = 'scatter',
+                mode = 'markers',
+                legendgroup = ~Molecule_List,
+                legendgrouptitle = list(text = ~Molecule_List),
+                showlegend = FALSE,
+                name = ~paste(PCA, "(points)"),
+                text = ~paste(
+                    "Molecule:", Molecule,
+                    "<br>Area:", round(Area, 2),
+                    "<br>Concentration:", round(Analyte_Concentration, 2),
+                    "<br>R2:", round(rsquared, 3)
+                ),
+                hoverinfo = 'text'
+            ) |>
+            plotly::add_trace(
+                data = group_data,
+                x = ~Analyte_Concentration,
+                y = ~RF * Analyte_Concentration + intercept,
+                color = ~PCA,
+                type = 'scatter',
+                mode = 'lines',
+                legendgroup = ~Molecule_List,
+                name = ~PCA,
+                hoverinfo = 'none'
+            )
+    }
+
+    # Calculate layout
+    subplot_cols <- min(length(groups), 2)  # Maximum 2 columns
+    subplot_rows <- ceiling(length(groups) / subplot_cols)
+
+    # Create annotations for titles
+    annotations <- list()
+    for(i in seq_along(groups)) {
+        row <- ceiling(i/subplot_cols)
+        col <- if(i %% subplot_cols == 0) subplot_cols else i %% subplot_cols
+
+        annotations[[i]] <- list(
+            text = groups[i],
+            font = list(size = 14),
+            xref = "paper",
+            yref = "paper",
+            x = (col - 0.5)/subplot_cols,
+            y = 1 - (row - 1)/subplot_rows,
+            xanchor = "center",
+            yanchor = "bottom",
+            showarrow = FALSE
+        )
+    }
+
+    # Combine plots using subplot
+    final_plot <- plotly::subplot(
+        plot_list,
+        nrows = subplot_rows,
+        shareX = FALSE,
+        #shareY = TRUE,
+        shareY = FALSE,
+        margin = 0.1
+    ) |>
+        plotly::layout(
+            height = 400 * subplot_rows,
+            showlegend = TRUE,
+            annotations = annotations,
+            margin = list(t = 50, b = 50, l = 50, r = 50),
+            legend = list(
+                groupclick = "togglegroup",  # Changed from "toggleitem" to "togglegroup"
+                tracegroupgap = 10,
+                itemsizing = "constant"
+            ),
+            xaxis = list(title = paste0("Analyte Concentration/Amount (", quantUnit, ")"))  # Use the reactive value for x-axis label
+        )
+
+    return(final_plot)
+}
+
+#############################################################################
+
+plot_quanqualratio <- function(Skyline_output_filt) {
+
+Skyline_output_filt |>
+    dplyr::group_by(Replicate_Name, Molecule) |>
+    dplyr::mutate(Quan_Area = ifelse(Isotope_Label_Type == "Quan", Area, NA)) |>
+    tidyr::fill(Quan_Area, .direction = "downup") |>
+    dplyr::mutate(QuanMZ = ifelse(Isotope_Label_Type == "Quan", Chromatogram_Precursor_MZ, NA)) |>
+    tidyr::fill(QuanMZ, .direction = "downup") |>
+    dplyr::mutate(QuanQualRatio = ifelse(Isotope_Label_Type == "Qual", Quan_Area/Area, 1)) |>
+    tidyr::replace_na(list(QuanQualRatio = 0)) |>
+    dplyr::mutate(QuanQualMZ = paste0(QuanMZ,"/",Chromatogram_Precursor_MZ)) |>
+    dplyr::ungroup() |>
+    dplyr::select(Replicate_Name, Sample_Type, Molecule_List, Molecule, QuanQualMZ, QuanQualRatio) |>
+    plotly::plot_ly(x = ~Replicate_Name, y = ~QuanQualRatio, type = 'violin', color = ~Sample_Type,
+            text = ~paste("Sample: ", Replicate_Name,
+                          "<br>Molecule List: ", Molecule_List,
+                          "<br>Molecule: ", Molecule,
+                          "<br>Quan/Qual MZ: ", QuanQualMZ,
+                          "<br>Ratio: ", round(QuanQualRatio, 2)),
+            hoverinfo = "text") |>
+    plotly::layout(title = 'Quan-to-Qual Ratio',
+           xaxis = list(title = 'Replicate Name'),
+           yaxis = list(title = 'Quan-to-Qual Ratio'))
+}
+
+##############################################################################
+
+plot_meas_vs_theor_ratio <- function(Skyline_output_filt) {
+
+    Skyline_output_filt |>
+        dplyr::group_by(Replicate_Name, Molecule) |>
+        dplyr::mutate(Quan_Area = ifelse(Isotope_Label_Type == "Quan", Area, NA)) |>
+        tidyr::fill(Quan_Area, .direction = "downup") |>
+        dplyr::mutate(QuanMZ = ifelse(Isotope_Label_Type == "Quan", Chromatogram_Precursor_MZ, NA)) |>
+        tidyr::fill(QuanMZ, .direction = "downup") |>
+        dplyr::mutate(QuanQualRatio = ifelse(Isotope_Label_Type == "Qual", Quan_Area/Area, 1)) |>
+        tidyr::replace_na(list(QuanQualRatio = 0)) |>
+        dplyr::mutate(QuanQualMZ = paste0(QuanMZ,"/",Chromatogram_Precursor_MZ)) |>
+
+        dplyr::mutate(Quan_Rel_Ab = ifelse(Isotope_Label_Type == "Quan", Rel_Ab, NA)) |>
+        tidyr::fill(Quan_Rel_Ab, .direction = "downup") |>
+        dplyr::mutate(QuanQual_Rel_Ab_Ratio = ifelse(Isotope_Label_Type == "Qual", Quan_Rel_Ab/Rel_Ab, 1)) |>
+        tidyr::replace_na(list(QuanQual_Rel_Ab_Ratio = 0)) |>
+        dplyr::ungroup() |>
+        dplyr::mutate(MeasVSTheo = QuanQualRatio/QuanQual_Rel_Ab_Ratio) |>
+        dplyr::mutate(Is_Outlier = MeasVSTheo > 3 | MeasVSTheo < 0.3) |>
+        dplyr::select(Replicate_Name, Sample_Type, Molecule_List, Molecule, QuanQualMZ, QuanQualRatio, QuanQual_Rel_Ab_Ratio, MeasVSTheo, Is_Outlier) |>
+        plotly::plot_ly(x = ~Replicate_Name, y = ~MeasVSTheo,
+                        type = 'scatter', mode = 'markers',
+                        color = ~Is_Outlier,
+                        colors = c('blue', 'red'),
+                        text = ~paste("Replicate:", Replicate_Name,
+                                      "<br>Homologue Group: ", Molecule,
+                                      "<br>Measured against Theoretical Ratio:", round(MeasVSTheo, 1)),
+                        marker = list(size = 10)) %>%
+        layout(title = "Measured/Theoretical ratio >3 or <0.3 are marked in red)",
+               xaxis = list(title = "Sample Name"),
+               yaxis = list(title = "MeasVSTheo"))
+
+}
+
+
+##############################################################################
+
+plot_sample_contribution <- function(deconvolution) {
+
+    # How much contribution of each sample to the final deconvoluted homologue group pattern
+    plot_data <- deconvolution |>
+        tidyr::unnest(deconv_coef) |>
+        tidyr::unnest_longer(c(deconv_coef, Batch_Name)) |>
+        dplyr::select(Replicate_Name, Batch_Name, deconv_coef)
+
+    # Create the plotly stacked bar plot
+    plotly::plot_ly(plot_data,
+            x = ~Replicate_Name,
+            y = ~deconv_coef,
+            type = "bar",
+            color = ~Batch_Name,
+            colors = "Spectral") |>
+        plotly::layout(
+            title = list(
+                text = "Contributions from standards to deconvoluted homologue pattern",
+                x = 0.5,  # Center the title
+                y = 0.95  # Position slightly down from top
+            ),
+            barmode = "stack",
+            xaxis = list(title = "Replicate Name"),
+            yaxis = list(title = "Relative Contribution",
+                         tickformat = ".2%"),
+            showlegend = TRUE,
+            legend = list(title = list(text = "Batch Name"))
+        )
+}
+
+#############################################################################
+
+plot_homologue_group_pattern_comparison <- function(Sample_distribution, input_selectedSamples){
+
+    # Filter data for selected samples and reshape data
+    selected_samples <- Sample_distribution |>
+        dplyr::filter(Replicate_Name %in% input_selectedSamples) |>
+        dplyr::mutate(Molecule = factor(Molecule, levels = unique(Molecule[order(C_number, Cl_number)])))
+
+    # Get unique homologue groups for consistent coloring
+    homologue_groups <- unique(selected_samples$C_homologue)
+
+    # Create a list of plots, one for each Replicate_Name
+    plot_list <- selected_samples |>
+        split(selected_samples$Replicate_Name) |>
+        map(function(df) {
+            # Create base plot
+            p <- plotly::plot_ly()
+
+            # Add bars for each homologue group
+            for(hg in homologue_groups) {
+                df_filtered <- df[df$C_homologue == hg,]
+                p <- p |>
+                    plotly::add_trace(
+                        data = df_filtered,
+                        x = ~Molecule,
+                        y = ~Relative_Area,
+                        name = hg,
+                        legendgroup = hg,
+                        showlegend = (df_filtered$Replicate_Name[1] == input_selectedSamples[1]),
+                        type = 'bar',
+                        opacity = 1
+                    )
+            }
+
+            # Add the black line for resolved_distribution
+            p <- p |>
+                plotly::add_trace(
+                    data = df,
+                    x = ~Molecule,
+                    y = ~resolved_distribution,
+                    name = "Deconvoluted Distribution",
+                    legendgroup = "DeconvDistr",
+                    showlegend = (df$Replicate_Name[1] == input_selectedSamples[1]),
+                    type = 'scatter',
+                    mode = 'lines+markers',
+                    line = list(color = 'black'),
+                    marker = list(color = 'black', size = 6),
+                    opacity = 0.7
+                )
+
+            # Add layout
+            p <- p |>
+                plotly::layout(
+                    xaxis = list(
+                        title = "Homologue",
+                        tickangle = 45
+                    ),
+                    yaxis = list(title = "Value"),
+                    barmode = 'group',
+                    annotations = list(
+                        x = 0.5,
+                        y = 1.1,
+                        text = unique(df$Replicate_Name),
+                        xref = 'paper',
+                        yref = 'paper',
+                        showarrow = FALSE
+                    )
+                )
+
+            return(p)
+        })
+
+    # Combine the plots using subplot
+    plotly::subplot(plot_list,
+                    nrows = ceiling(length(plot_list)/2),
+                    shareX = TRUE,
+                    shareY = TRUE) |>
+        plotly::layout(
+            #title = "Sample Comparison",
+            showlegend = TRUE,
+            hovermode = 'closest',
+            hoverlabel = list(bgcolor = "white"),
+            barmode = 'group'
+        ) |>
+        plotly::config(displayModeBar = TRUE) |>
+        htmlwidgets::onRender("
+                function(el) {
+                    var plotDiv = document.getElementById(el.id);
+                    plotDiv.on('plotly_legendclick', function(data) {
+                        Plotly.restyle(plotDiv, {
+                            visible: data.data[data.curveNumber].visible === 'legendonly' ? true : 'legendonly'
+                        }, data.fullData.map((trace, i) => i).filter(i =>
+                            data.fullData[i].legendgroup === data.fullData[data.curveNumber].legendgroup
+                        ));
+                        return false;
+                    });
+                }
+            ")
+}
+
---
+++ inst/instructions_CPions.md
@@ -0,0 +1,98 @@
+
+# Introduction
+This app generates a list of mass-over-charges (m/z) for PCAs and structural analogues to investigate potential overlapping m/z during mass spectrometric analysis.
+It utilizes the isopattern function of the R package Envipat (Loos et al, Analytical Chemistry, 2015, 87, 5738-5744) to generate isotopic patterns from chemical formula and adducts. The app will also generate a list of m/z for selected adducts which can be used as a transition list in the Skyline software for quantification purposes.
+Several adducts have been included and more can be added upon request.  
+  
+## Instructions    
+Choose the parameters in the _Normal settings_ or _Advanced settings_ tab. Press submit and wait for calculation to finish. A table will then be generated with all ions that conform with the initial setting parameters. The table can be exported to excel by clicking on the "Excel" button at the top.  
+The _Interfering ions_ tab can be used to check for ions that interfer with each other at the set resolution of the mass spectrometer. Default is set to R=60,000. The plots and tables are interactive and the user can filter the _"Interference at MS res?"_ by clicking on _FALSE_ on the plot legend (and thereby keeping all _TRUE_ ions, which will remove all ions that can be resolved by the set MS resolution).  
+The generated ion tables can be exported using the Skyline tab for data processing of MS data.  
+
+
+## Normal settings tab
+  
+__C atoms min__ and __C atoms max__: the range of carbon atoms.
+  
+__Cl atoms min__ and __Cl atoms max__: the range of chlorine atoms.  
+
+__Br atoms min__ and __Br atoms max__: the range of bromine atoms. This is only used if BCA (bromo-chloro alkanes) is choosen as adducts (otherwise this parameter can be ignored).
+  
+__Add adducts/fragments__: refers to the formula of adducts and/or fragments to generate from a set list of available options. Multiple selections are possible.  
+  
+__[PCA]__ and __[PCO]__: refers to the main groups of polychlorinated alkanes [PCA] or polychlorinated alkenes (mono olefins) [PCO]. 
+
+__[BCA]__: bromo-chloro alkanes.
+  
+__[xxx-yy]__ or __[xxx-yy-zz]__: where _xxx_ refers to the main groups, _-yy_ refers to the adduct/fragment ions or _-yy-zz_ which are the consecutive loss fragments. Currently, a limited selection is available but more can be added upon request.  
+  
+__[ ]-__ and __[ ]+__ refers to the charge of the ion (limited to single charged species, +1 or -1).  
+  
+_Note that [M+Cl-HCl]- can also be written as [M-H]-_  
+  
+__Isotope rel ab threshold (%)__: is the threshold for relative abundance for isotopologues for each chemical formula of the adduct/fragment ion. Ions below this threshold will not be included into the generated ion table.
+  
+__Optional: add ion formula for IS/RS__: input the ion formula for IS or RS if needed. Enter one formula per line. Indicate IS or RS isotopic formula and charge separated by space. _The ion formula should be for the adduct ion_ and not the neutral chemical formula.  
+The input for the ion formula is written in three parts, each separated by a `blank space`, the IS or RS, the ion formula where heavy isotopes are indicated by [13]C or [2]H, and the charge (+ or -).  
+Example:  
+IS [13]C10H16Cl7 -  
+RS [13]C12H18Cl9 -  
+_(make sure there is no empty last line)_  
+
+The first line will product the m/z for the [M+Cl]- adduct ion of the IS with the formula ^13^C~10~H~16~Cl~6~, while the second line is for the [M+Cl]- adduct ion for the ^13^C~12~H~18~Cl~8~ standard.  
+
+  
+## Advanced settings tab  
+Mostly same initial parameters as Normal settings. In advanced settings, there is more flexibility to combine and mix the `Compound Class`, `Adduct`, `Charge`, and `Transformation product`.  
+  
+### Output table  
+  
+__Parent_Formula__: the chemical formula of the molecular ion.  
+
+__Halo_perc__: for PCA, PCO chlorinated groups, the chlorination degree (mass percentage of Cl). If BCA, then it is the combined Cl+Br percentage.
+  
+__Charge__: The charge of the ion.  
+  
+__Fragment__: The fragment and isotopic type of the ion species.  
+  
+__Adduct_Formula__: the chemical formula of the adduct/fragment ion.  
+  
+__Isotopologue__: the isotopologue in relation to the monoisotopic ion.  
+  
+__Isotope_Formula__: the exact isotopic formula of the adduct/fragment ion.  
+  
+__m/z__: the mass-over-charge of the adduct/fragment ion.  
+  
+__Rel_ab__: the relative abundance of the different isotopologues of each adduct/fragment ion.  
+  
+__12C__, __13C__, __1H__, __2H__, __35Cl__, __37Cl__, __79Br__, __81Br__: the number of atoms for each element
+  
+## Interfering ions tab  
+  
+__difflag__, __difflead__: internal calculations for the difference in m/z values between the two nearest ions. If "interference at MS res?" filter has been used, then the previous/next ions might not be shown. 
+  
+__reslag__, __reslead__: internal calculations for the MS resolution needed to separate the two nearest ions. If "interference at MS res?" filter has been used, then the previous/next ions might not be shown.  
+  
+__interference__: indicate whether or not the m/z two nearest ions can interfere with each other at the set MS resolution value. _"false"_ means no interference and _"true"_ means there is interference (and therefore the MS resolution cannot resolve these peaks).  
+
+
+  
+  
+### Plot outputs  
+  
+Note: Some bars can exceed 100% in relative abundance in the y-axis, and this indicates that some isotopologues have the exact same ion formula. You can hover over the different segments of these bars to check the overlapping mass ions.
+
+__Hover text__:   
+__difflag & difflead (prev and next)__: the difference between the m/z with previous and next ions (axis ordered from lowest to highest m/z).  
+__reslag & reslead (prev and next)__: the MS resolution needed to resolve previous and next ion (axis ordered from lowest to highest m/z).  
+  
+  
+## Skyline tab  
+
+This will output a transition list for import in Skyline for integration and quantification. User need to indicate either the _Normal setting_ or _Advanced setting_ as input.  
+
+  
+  
+
+
+
